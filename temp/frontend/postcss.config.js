3a2cdc504166c8707ec26769e6845676461edcb3
apodemus_agrarius <reanimator737@gmail.com>
2024-09-04 10:50:04 +0200

    pause

--- temp/frontend/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('@tailwindcss/deprecation-warnings'),
-        require('autoprefixer'),
-    ],
-};
